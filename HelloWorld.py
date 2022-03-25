02d18ba5ff9a891b2aed82115f81b449d820cd11
Prerit Dayal <62067658+prerit714@users.noreply.github.com>
2022-03-25 10:30:51 +0530

    Second commit to this repo

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,19 @@
 print("Hello world!")
+
+
+class Human:
+
+    def __init__(self, name, age, is_male=True):
+        self.name = name
+        self.age = age
+        self.is_male = is_male
+    
+    def __str__(self):
+        return "Human<name={}, age={}, is_male={}>".format(
+            self.name,
+            self.age,
+            self.is_male
+        )
+
+prerit = Human("Prerit Dayal", 22)
+print(prerit)
